efaee23229fb929d9b7d1e75e67141aff535cca0
Gustavo Coronel <gcoronelc@gmail.com>
2022-05-31 03:10:20 -0500

    Clase 4

--- Clase03-29052022/AplicandoLambda/src/aplicandolambda/prueba/Prueba.java
+++ Clase03-29052022/AplicandoLambda/src/aplicandolambda/prueba/Prueba.java
@@ -9,5 +9,20 @@ package aplicandolambda.prueba;
  * @cursos gcoronelc.github.io
  */
 public class Prueba {
+	
+	public static void main(String[] args) {
+		
+		int[][] datos3 = {{54,65,23},{54,78,},{23,76,34,65},{12,45,67,32}};
+		
+		// Recorrido tipo indexado
+		for (int[] fila : datos3) {
+			for (int j = 0; j < fila.length; j++) {
+				int dato = fila[j];
+				System.out.print("\t" + dato);
+			}
+			System.out.println("");
+		}
+		
+	}
 
 }
