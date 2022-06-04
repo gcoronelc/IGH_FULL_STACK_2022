f61d835d7db1385cc878111aadf5c6fede4ad134
Gustavo Coronel <gcoronelc@gmail.com>
2022-06-03 21:29:42 -0500

    Clase 05

---
+++ Clase05-03062022/AprendiendoJava/src/aprendiendojava/prueba/Prueba05.java
@@ -0,0 +1,16 @@
+package aprendiendojava.prueba;
+
+/**
+ * @author Eric Gustavo Coronel Castillo
+ * @blog www.desarrollasoftware.com
+ * @email gcoronelc@gmail.com
+ * @youtube www.youtube.com/DesarrollaSoftware
+ * @facebook www.facebook.com/groups/desarrollasoftware
+ * @cursos gcoronelc.github.io
+ */
+public class Prueba05 {
+
+	public static void main(String[] args) {
+		System.out.println("El mayor de 15 y 20 es: " + Math.max(15, 20));
+	}
+}
