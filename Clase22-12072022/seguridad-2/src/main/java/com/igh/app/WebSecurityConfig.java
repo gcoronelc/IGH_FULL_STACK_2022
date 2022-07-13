9e939102d1c5975b35029db85109461fe43a845a
Gustavo Coronel <gcoronelc@gmail.com>
2022-07-12 21:29:34 -0500

    Clase 22

---
+++ Clase22-12072022/seguridad-2/src/main/java/com/igh/app/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.igh.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.anyRequest()
+		.authenticated()
+		.and().
+		formLogin();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.passwordEncoder(new BCryptPasswordEncoder()).withUser("gustavo")
+		.password(passwordEncoder().encode("123456")).roles("ADMIN").
+		and().withUser("reader")	
+		.password(passwordEncoder().
+		encode("123456")).roles("USER");
+	}
+
+}
---
+++ Clase22-12072022/seguridad-2/src/main/java/com/igh/app/controller/AppController.java
@@ -0,0 +1,13 @@
+package com.igh.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+
+	@RequestMapping({ "/", "/home" })
+	public String home() {
+		return "home";
+	}
+}
