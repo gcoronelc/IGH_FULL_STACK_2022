a31c89d8a9d539dee5ca657a7f079a247a353213
Gustavo Coronel <gcoronelc@gmail.com>
2022-07-17 19:15:04 -0500

    Clase 24

--- Clase24-17072022/Ejemplo3/demoRestCrud/src/main/java/com/igh/app/controller/ClienteController.java
+++ Clase24-17072022/Ejemplo3/demoRestCrud/src/main/java/com/igh/app/controller/ClienteController.java
@@ -1,10 +1,19 @@
 package com.igh.app.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +33,55 @@ public class ClienteController {
 		return clienteService.traerTodos();
 	}
 
+	@GetMapping("/paterno/{paterno}")
+	public List<Cliente> traerPorPaterno(@PathVariable("paterno") String paterno){
+		return clienteService.traerPorPaterno(paterno);
+	}
+	
+	@GetMapping("/id/{id}")
+	public ResponseEntity<Cliente> traerPorId(@PathVariable("id") String id){
+		Optional<Cliente> objCliente = clienteService.traerPorId(id);
+		if(!objCliente.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(objCliente.get());
+	}
+	
+	@PostMapping
+	public ResponseEntity<Cliente> crear(@RequestBody Cliente cliente){
+		Cliente objCli = clienteService.crear(cliente);
+		return ResponseEntity.status(HttpStatus.CREATED).body(objCli);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Cliente> actualizar(@RequestBody Cliente cliente){
+		Optional<Cliente> objCli = clienteService.traerPorId(cliente.getCodigo());
+		// Verificar si existe
+		if(!objCli.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		// Tecnica campo x campo
+		objCli.get().setPaterno(cliente.getPaterno());
+		objCli.get().setMaterno(cliente.getMaterno());
+		// Todos los campos de una vez
+		BeanUtils.copyProperties(cliente, objCli.get());
+		// Actualizar la base de datos
+		clienteService.actualizar(objCli.get());
+		// Grabar y finalizar
+		return ResponseEntity.status(HttpStatus.CREATED).body(objCli.get());
+	}
+	
+	@DeleteMapping("/id/{id}")
+	public ResponseEntity<Cliente> eliminar(@PathVariable("id") String id){
+		Optional<Cliente> objCli = clienteService.traerPorId(id);
+		// Verificar si existe
+		if(!objCli.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		// Eliminar
+		clienteService.eliminar(id);
+		// Grabar y finalizar
+		return ResponseEntity.status(HttpStatus.CREATED).body(objCli.get());
+	}
+	
 }
--- Clase24-17072022/Ejemplo3/demoRestCrud/src/main/java/com/igh/app/service/ClienteService.java
+++ Clase24-17072022/Ejemplo3/demoRestCrud/src/main/java/com/igh/app/service/ClienteService.java
@@ -1,12 +1,15 @@
 package com.igh.app.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.igh.app.domain.Cliente;
+import com.igh.app.domain.Contador;
 import com.igh.app.repository.ClienteRepository;
 import com.igh.app.repository.ContadorRepository;
 
@@ -23,4 +26,39 @@ public class ClienteService {
 		return (List<Cliente>) clienteRepository.findAll();
 	}
 
+	public List<Cliente> traerPorPaterno(String paterno){	
+		return clienteRepository.findByPaternoContainingIgnoreCase(paterno);
+	}
+	
+	public Optional<Cliente> traerPorId(String id){
+		return clienteRepository.findById(id);
+	}
+	
+	@Transactional(rollbackFor = Exception.class,propagation = Propagation.REQUIRES_NEW)
+	public Cliente crear(Cliente cliente) {
+		// Codigo
+		Optional<Contador> contador = contadorRepository.findById("Cliente");
+		int siguiente = contador.get().getContador() + 1;
+		int longitud = contador.get().getLongitud();
+		String formato = "%0" + longitud + "d";
+		String codigo = String.format(formato, siguiente);
+		// Actualiza el contador
+		contador.get().setContador(siguiente);
+		// Registrar el cliente
+		cliente.setCodigo(codigo);
+		Cliente objCliente = clienteRepository.save(cliente);
+		// Fin
+		return objCliente;
+	}
+	
+	@Transactional(rollbackFor = Exception.class)
+	public Cliente actualizar(Cliente cliente) {
+		Cliente objCliente = clienteRepository.save(cliente);
+		return objCliente;
+	}
+	
+	@Transactional(rollbackFor = Exception.class)
+	public void eliminar(String id) {
+		clienteRepository.deleteById(id);
+	}
 }
